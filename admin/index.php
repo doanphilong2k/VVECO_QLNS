cdaed9b03d18a1f60769d749e945cff90384e7fb
tuankiet116 <57905166+tuankiet116@users.noreply.github.com>
2021-02-18 16:18:28 +0700

    not yet

--- admin/index.php
+++ admin/index.php
@@ -23,8 +23,9 @@ if($row	= mysqli_fetch_assoc($db_language->result)){
 	$_SESSION["lang_id"] 	= $lang_id;
 }
 
-$isAdmin		= isset($_SESSION["isAdmin"]) ? intval($_SESSION["isAdmin"]) : 0;
+$isAdmin	= isset($_SESSION["isAdmin"]) ? intval($_SESSION["isAdmin"]) : 0;
 $loginName	= isset($_SESSION["userlogin"]) ? $_SESSION["userlogin"] : "";
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
