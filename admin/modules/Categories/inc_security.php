c62f8ab4b6ae1fea96134d0e91908f76b7a6096f
tuankiet116 <kietdat1612000@gmail.com>
2021-05-07 11:34:56 +0700

    not finish

--- admin/modules/Categories/inc_security.php
+++ admin/modules/Categories/inc_security.php
@@ -6,7 +6,4 @@ checkLogged();
 //Check access module...
 if (checkAccessModule($module_id) != 1) redirect($fs_denypath);
 //Declare prameter when insert data
-$fs_table = "workshift";
-$id_field = "wor_idShift";
-$name_field = "wor_Name";
 ?>
